ac7c3bf78d3c86b7333989d36693d1d040f0eee8
Ben Stone <benjamin.h.stone@gmail.com>
2018-05-11 10:34:06 -0400

    Initial Commit

---
+++ Objective-C-ClassIntro/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Objective-C-ClassIntro
+//
+//  Created by C4Q  on 5/11/18.
+//  Copyright © 2018 C4Q . All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
